2d293886ea5b404db419b385745b206a887f0178
daviepineda1 <87446052+daviepineda1@users.noreply.github.com>
2022-01-26 05:37:53 -0700

    Update Dealer.cs

--- Dealer.cs
+++ Dealer.cs
@@ -8,6 +8,10 @@ namespace HiLo
         public int _userPoints = 300;
         public string _userPlay = "Y";
 
+        Random _rand = new Random();
+        List<int> _cardNumbers = new List<int>();
+
+
 
         public void DisplayInformation()
         {
